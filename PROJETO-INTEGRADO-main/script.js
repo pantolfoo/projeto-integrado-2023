37775a8a39a4ccee2434cefb1a1c7d761d52d123
LucxsMilhomem <126785721+LucxsMilhomem@users.noreply.github.com>
2023-11-13 02:38:42 -0300

    Add files via upload

--- PROJETO-INTEGRADO-main/script.js
+++ PROJETO-INTEGRADO-main/script.js
@@ -11,3 +11,24 @@ function redirecionarCardapio()
 document.getElementById('login-button').addEventListener('click', redirecionarProximaPagina);
 // Adicione um ouvinte de evento ao botão de login
 
+function Mudarestado(el,il,bel,bil) {
+        document.getElementById(el).style.display = 'block';
+        document.getElementById(il).style.display = 'none';
+        document.getElementById(bel).style.backgroundColor = "#ff0000";
+        document.getElementById(bel).style.color = "#ffffff";
+        document.getElementById(bel).style.borderColor = "#ff0000";
+        document.getElementById(bil).style.backgroundColor = "#ffffff";
+        document.getElementById(bil).style.color = "#ff0000";
+        document.getElementById(bil).style.borderColor = "#ffffff";
+}
+
+function Mudarestado2(el,il,bil,bel) {
+    document.getElementById(il).style.display = 'block';
+    document.getElementById(el).style.display = 'none';
+    document.getElementById(bel).style.backgroundColor = "#ff0000";
+    document.getElementById(bel).style.color = "#ffffff";
+    document.getElementById(bel).style.borderColor = "#ff0000";
+    document.getElementById(bil).style.backgroundColor = "#ffffff";
+    document.getElementById(bil).style.color = "#ff0000";
+    document.getElementById(bil).style.borderColor = "#ffffff";
+}
